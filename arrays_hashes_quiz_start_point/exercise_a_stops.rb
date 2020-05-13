a81f26ea521d1cfc81344901aacbc5922d4d6388
Jack Morris <morriseo94@gmail.com>
2020-05-13 17:27:46 +0100

    A9

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -1,5 +1,4 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-num = 0
 
 #1. Add "Edinburgh Waverley" to the end of the array
 stops.push("Edinburgh Waverley")
@@ -11,19 +10,17 @@ stops.unshift("Glasgow Queen St")
 stops.insert(4, "Polmont")
 
 #4. Print out the index position of "Linlithgow"
+print "Number stops for Linlithgow? "
 print stops.index("Linlithgow")
 #5. Remove "Livingston" from the array using its name
 stops.delete("Livingston")
 #6. Delete "Cumbernauld" from the array by index
 stops.delete_at(2)
 #7. Print the number of stops there are in the array?
-for stop in stops
-    num += 1
-end
-print "number of stops there are? #{num}" 
-
-
+print "  Num of stops all together?"
+print stops.length
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+stops[2]
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
 p stops

54c60a0344c35d5d72fe696f3f2a294e26e27fcd
Jack Morris <morriseo94@gmail.com>
2020-05-13 17:35:07 +0100

    A fin

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -12,15 +12,25 @@ stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
 print "Number stops for Linlithgow? "
 print stops.index("Linlithgow")
+
 #5. Remove "Livingston" from the array using its name
 stops.delete("Livingston")
+
 #6. Delete "Cumbernauld" from the array by index
 stops.delete_at(2)
+
 #7. Print the number of stops there are in the array?
-print "  Num of stops all together?"
+print "  Num of stops all together? "
 print stops.length
+
 #8. Show as many ways as you can to return "Falkirk High" from the array?
-stops[2]
+ stops[2]
+ stops.index("Falkirk High")
+
 #9. Reverse the positions of the stops in the array
+stops.reverse
+
 #10 Print out all the stops using a for loop
-p stops
+for stop in stops
+    print " /" + stop + " /"
+end
